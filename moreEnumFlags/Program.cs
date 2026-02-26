202c0ffa313b0db8a6cbf5d4014b296f3a8f911b
holden.anderson <holden.anderson@students.snow.edu>
2026-02-26 14:55:54 -0700

    initial commit notes from class

---
+++ moreEnumFlags/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+
+
+
+//when we sue the | it combines/add them 
+Powers hero = Powers.SuperStrength | Powers.Teleport;
+
+if (hero.HasFlag(Powers.SuperStrength))
+    {
+        Console.WriteLine("Crushes his foe!");       
+    }
+if (hero.HasFlag(Powers.Flight))
+    {
+        Console.WriteLine("Flies around");       
+    }
+if (hero.HasFlag(Powers.LaserVision))
+    {
+        Console.WriteLine("Melting candles");       
+    }
+if (hero.HasFlag(Powers.Teleport))
+    {
+        Console.WriteLine("Teleports around");       
+    }
+if (hero.HasFlag(Powers.AllMight))
+    {
+        Console.WriteLine("Incredible catchphrase activate!");       
+    }
+
+[Flags] //attribute - a label that other code can make decision with
+//ye, flags make our code extra fancy
+enum Powers
+{
+    None = 0,
+    SuperStrength = 1,
+    Flight = 2,
+    LaserVision = 4,
+    Teleport = 8, 
+    AllMight = 16,
+
+}
+
+
+/* 
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello World");
+	}
+} */
